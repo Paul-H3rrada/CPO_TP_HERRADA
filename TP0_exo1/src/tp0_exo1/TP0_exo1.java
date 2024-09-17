5be701c248b9547df9306cadcdf9fcfd8e6f3e43
Paul Herrada <paul.herrada@epfedu.fr>
2024-09-17 17:07:51 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_exo1/src/tp0_exo1/TP0_exo1.java
@@ -0,0 +1,27 @@
+/*
+Nom:Herrada
+TDC
+TP0_Exo1
+17/09/2024
+ */
+package tp0_exo1;
+
+import java.util.Scanner;
+
+/*
+*/
+public class TP0_exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        sc.nextLine();
+        System.out.println("Au Revoir!");
+    }
+    
+}
