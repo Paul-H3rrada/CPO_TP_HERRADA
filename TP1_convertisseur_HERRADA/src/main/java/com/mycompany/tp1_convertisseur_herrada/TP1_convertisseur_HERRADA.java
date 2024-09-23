b38f651158099abf6cad79c233d98d9ed5890cbe
Paul Herrada <paul.herrada@epfedu.fr>
2024-09-23 11:15:43 +0200

    Seéance : J'ai finis le TP0 et commencé le TP1

---
+++ TP1_convertisseur_HERRADA/src/main/java/com/mycompany/tp1_convertisseur_herrada/TP1_convertisseur_HERRADA.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp1_convertisseur_herrada;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author herra
+ */
+public class TP1_convertisseur_HERRADA {
+
+    public static void main(String[] args) {
+    double a;
+    double b;
+    double Kel;
+    Scanner sc=new Scanner(System.in);
+    
+    System.out.println("Choisissez un nombre");
+    a=sc.nextDouble();
+    System.out.println("Votre nombre choisi est "+a +"");
+    Kel=273.15+a;
+    System.out.println("Ceci nous donne une Temperature en Kelvin de "+Kel +"");
+    }
+    public static double CelciusVersKelvin (double tCelcius) { 
+    a+273.15
+    }
+}
+
